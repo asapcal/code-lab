b7b3bd247d72c810800c6b2fc19c618df4ea75c5
Asaph Caldeira <asaphcaldeira@gmail.com>
2024-06-11 12:53:02 -0300

    adding python lessons

---
+++ yt-lab/brocode/python-full/lessons.py
@@ -0,0 +1,82 @@
+#Python lessons
+
+#Intro
+
+#print("I love pizza, it's my favorite food")
+
+#Variables
+
+#variable = a container for a value. Behaves as the value that it contains
+
+#first_name = "Asaf"
+#last_name = "Looper"
+#full_name = first_name + " " + last_name
+#print(type(name))
+#print("Hello "+full_name+"!!")
+
+#age = 26
+#age += 1
+#print("Your age is: "+str(age))
+#print(type(age))
+
+#height = 1.85
+#weight = 75
+#bmi = weight / height**2
+#print("Your BMI is: "+str(bmi))
+#print(type(height))
+
+#human = False
+#print(human)
+#print("Are you a human: "+str(human))
+#print(type(human))
+
+#Multiple variables
+
+#multiple assignment = allows us to assign multiple variables at the same time in one line of code
+
+#name = "Bro"
+#age = 21
+#attractive = True
+#print(name)
+#print(age)
+#print(attractive)
+
+#name, age, attractive = "Bro", 21, True
+#print(name, age, attractive)
+
+#SpongeBob = Patrick = Sandy = Squidward = 30
+#print(SpongeBob, Patrick, Sandy, Squidward)
+
+#String methods
+
+#name = "asaf"
+
+#print(name)
+#print(len(name))
+#print(name.find("a"))
+#print(name.capitalize())
+#print(name.upper())
+#print(name.lower())
+#print(name.isdigit())
+#print(name.isalpha())
+#print(name.count("a"))
+#print(name.replace("a", "e"))
+#print(name*3)
+
+#Lists
+
+#names = ["Asaf", "Bro", "Patrick", "SpongeBob", "Sandy"]
+#print(names)
+#print(names[1])
+#print(names[-1])
+#print(names[0:3])
+
+#Type casting = convert the data type of a value to another data type.
+
+x = 1 #int
+y = 2.0 #float
+z = "3" #str
+
+print(x)
+print(y)
+print(z)
