02eed7c7fa9fe5f9c2ecd91f5ab562d721f00edd
Asaph Almeida <asax.cal.97@gmail.com>
2024-06-18 08:47:10 -0300

    add/remove: test and lab files

--- yt-lab/brocode/python-full/lessons.py
+++ yt-lab/brocode/python-full/lessons.py
@@ -91,17 +91,57 @@
 
 #user imput
 
-name = input("What is your name?: ")
-age = int(input("How old are you?: "))
-height = float(input("How tall are you?: "))
+#name = input("What is your name?: ")
+#age = int(input("How old are you?: "))
+#height = float(input("How tall are you?: "))
 
-print("Hello "+name)
-print("You are "+str(age)+" years old")
-print("You are "+str(height)+"cm tall")
+#print("Hello "+name)
+#print("You are "+str(age)+" years old")
+#print("You are "+str(height)+" cm tall")
+
+#math functions
+
+#import math
+
+#pi = 3.14
+#x = 1
+#y = 2
+#z = 3
+
+#print(round(pi))
+#print(math.ceil(pi))
+#print(math.floor(pi))
+#print(abs(pi))
+#print(pow(pi,2))
+#print(math.sqrt(430))
+#print(max(x,y,z))
+#print(min(x,y,z))
 
 
+# slicing = create a substring by extracting elements from another string
+#           indexing[] or slice()
+#           [start:stop:step]
 
+#name = "Bro Code"
 
+#first_name = name[0:3]
+#last_name = name[4:9]
+#funky_name = name[0:9:3]
+#reversed_name = name[::-1]
+#print(first_name)
+#print(last_name)
+#print(funky_name)
+#print(reversed_name)
+
+website1 = "https://google.com"
+website2 = "https://wikipedia.com"
+
+slice = slice(8,12)
+print(website2[slice])
+
+# if statement = a block of code that will execute if a condition is true
+
+age = int(input("What is your age?: "))
 
 
 
