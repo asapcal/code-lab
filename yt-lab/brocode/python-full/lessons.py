c589a86f94fb19ae107754ffca009db03eec2a79
Asaph Almeida <asax.cal.97@gmail.com>
2024-06-20 13:04:52 -0300

    feat: Add age-based greeting functionality
    Introduce a new section of code to greet users based on their age, providing specific messages for various age ranges. This enhances the user experience by adding more personalized interactions.

--- yt-lab/brocode/python-full/lessons.py
+++ yt-lab/brocode/python-full/lessons.py
@@ -143,6 +143,17 @@ print(website2[slice])
 
 age = int(input("What is your age?: "))
 
+if age == 100:  
+    print("You are a century old!")
+elif age > 100:  
+    print("You are immortal?")
+elif age >= 18:
+    print("You are an adult!")
+elif age < 0:
+    print("You haven't been born yet!")
+else:
+    print("You are a child!")
+
 
 
 
