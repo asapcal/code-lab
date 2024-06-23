24cf1df62a04e8ea33f260623faeee2275499c4c
Asaph Caldeira <asaphcaldeira@gmail.com>
2024-06-23 00:54:27 -0300

    feat: Create basic web server with JSON response
    Adds a basic web server to return JSON data about a car. This introduces the ability to expose car data through a web API, laying the groundwork for future API development.

--- fullcycle/golang-first-program/hello/main.go
+++ fullcycle/golang-first-program/hello/main.go
@@ -1,5 +1,41 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+type Carro struct {
+	Nome   string
+	Modelo string
+	Ano    int
+}
+
+func (c Carro) Andar() {
+	fmt.Println("O carro " + c.Nome + " está andando")
+}
+
+func (c Carro) Parar() {
+	fmt.Println("O carro " + c.Nome + " está parando")
+}
+
 func main() {
-	println("Hello, World!")
+	//carro1 := Carro{Nome: "Ford", Modelo: "Mustang", Ano: 1969}
+	carro2 := Carro{Nome: "Chevrolet", Modelo: "Camaro", Ano: 1969}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		json.NewEncoder(w).Encode(carro2)
+	})
+
+	//carro2.Andar()
+	//carro1.Parar()
+
+	//fmt.Println(carro1.Nome)
+	//fmt.Println(carro2.Nome)
+
+	//fmt.Println(carro1.Nome)
+	//fmt.Println(carro2.Nome)
+
+	http.ListenAndServe(":8080", nil)
 }
